220f5452035c1985a2baead16cc7e66e04afba90
Tristan Davies <tristan@tristandavies.me>
2010-06-03 20:24:08 -0400

    Added "arr" class, which stands for array. It has a method called addkeys which can be called two ways: 1. n('arr',1,2,3)->addkeys('one','two','three')->print_r();    will show array('one' => 1, 'two' => 2, 'three' => 3); 2. n('arr',array('cake' => 4,'pie' => 2))->addkeys(array('cake' => 2,'pie' => 5, 'chicken' => 2))->print_r();    will show array('cake' => 4, 'pie' => 2, 'chicken' => 2);

---
+++ system/libraries/arr.php
@@ -0,0 +1,67 @@
+<?php
+class arr extends structure {
+private $data;
+	public function __construct($data = array())
+		{
+		call_user_func_array(array($this,'raw'),func_get_args());
+		$this->set_array($this->data);
+		}
+	public function raw($data = null)
+		{
+		if (func_num_args() == 0) {return $this->data;}
+		if (!is_array($data) || func_num_args() > 1)
+			{
+			$data = func_get_args();
+			}
+		$this->data = $data;
+		return $this;
+		}
+	public function addkeys($keys)
+		{
+		if (!is_array($keys) || func_num_args() > 1)
+			{
+			$keys = func_get_args();
+			}
+		$i = 0;
+		foreach ($keys as $key => $default)
+			{
+			if (is_numeric($key))
+				{
+				if (isset($this->data[$i]))
+					{
+					$this->data[$default] = $this->data[$i];
+					unset($this->data[$i]);
+					}
+				else
+					{
+					break;
+					}
+				}
+			else
+				{
+				if (!isset($this->data[$key]))
+					{
+					$this->data[$key] = $default;
+					}
+				}
+			}
+		return $this;
+		}
+	public function set($field,$value)
+		{
+		$this->data[$field] = $value;
+		return $this;
+		}
+	public function get($field)
+		{
+		return $this->data[$field];
+		}
+	public function print_r($return = false)
+		{
+		if ($return)
+			{
+			return print_r($this->data,true);
+			}
+		print_r($this->data);
+		}
+}
