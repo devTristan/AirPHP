0c0f754d23b8859c6f9fce1406a1a375fa6e7b68
Tristan Davies <tristan@tristandavies.me>
2010-06-22 22:22:12 -0400

    Added hex>rgb and rgb>hex color conversion methods

--- system/helpers/number.php
+++ system/helpers/number.php
@@ -1,6 +1,10 @@
 <?php
 class number extends helper {
 static private $times = array(
+	'picosecond' => 0.000000000001,
+	'nanosecond' => 0.000000001,
+	'microsecond' => 0.000001,
+	'millisecond' => 0.001,
 	'second' => 1,
 	'minute' => 60,
 	'hour' => 3600,
@@ -25,8 +29,26 @@ static private $times = array(
 		$out = implode($delim, $out);
 		return $out;
 		}
-	static public function format($number, $decimal = 3, $leading_zeroes = 0, $decimal_point = '.', $thousands_sep = ',')
+	public static function format($number, $decimal = 3, $leading_zeroes = 0, $decimal_point = '.', $thousands_sep = ',')
 		{
 		
+		}
+	public static function hex_to_rgb($hex)
+		{
+		return array(hexdec(substr($hex,0,2)),hexdec(substr($hex,2,2)),hexdec(substr($hex,4,2)));
+		}
+	public static function rgb_to_hex($rgb)
+		{
+		$hex = '';
+		foreach ($rgb as &$value)
+			{
+			$value = dechex($value);
+			if (strlen($value) == 1)
+				{
+				$value = $value.'0';
+				}
+			$hex .= $value;
+			}
+		return $hex;
 		}
 }
