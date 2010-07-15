e0ad8d5e0027051e477fb5f6d1634c5b5d1757c1
Tristan Davies <tristan@tristandavies.me>
2010-07-15 08:20:05 -0400

    Fixed bug that occured when calling from another directory

--- cli.php
+++ cli.php
@@ -7,10 +7,10 @@ description:
 	that the environment is sane. It then figures out which controller and
 	method to use, checks if they exist (if not 404ing), and executes them.
 */
-$overhead_start = microtime(true);
-error_reporting(E_ALL);
-$olddir = getcwd();
-chdir('../');
+//error_reporting(E_ALL);
+$old_dir = getcwd();
+chdir(substr(__FILE__, 0, strrpos(__FILE__, '/')+1));
+
 define('FRAMEWORK_NAME','AirPHP');
 define('FRAMEWORK_VERSION',0.7);
 require_once 'system/core/autoload.php';
@@ -19,15 +19,11 @@ require_once 'system/core/abstracts.php';
 require_once 'system/core/airphp.php';
 s('airphp');
 s('config');
-$overhead_end = microtime(true);
-s('timing')->play('total')->set('total',$overhead_end-$overhead_start);
-s('timing')->pause('overhead')->set('overhead',$overhead_end-$overhead_start);
-unset($overhead_start);
-unset($overhead_end);
 foreach (s('config')->autoload_classes as $class)
 	{
 	s($class);
 	}
 unset($class);
 s('event')->trigger('initialize');
-chdir($olddir);
+
+chdir($old_dir);
