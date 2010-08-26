e55a7d5918c4229bae199341656a622f5a05ed69
Tristan Davies <tristan@tristandavies.me>
2010-08-26 12:01:37 -0400

    Added tmp function which generates temporary filenames. Also added date-like byte calculation, eg files::kb(1) is 1024

--- system/helpers/files.php
+++ system/helpers/files.php
@@ -1,5 +1,16 @@
 <?php
 class files extends helper {
+private static $sizes = array(
+	'kb' => 1,
+	'mb' => 2,
+	'gb' => 3,
+	'tb' => 4,
+	'pb' => 5,
+	'eb' => 6,
+	'zb' => 7,
+	'yb' => 8
+	);
+private static $tmpfile_length = 5;
 	static public function ls($dir = null)
 		{
 		if ($dir === null) {$dir = getcwd();}
@@ -23,5 +34,17 @@ class files extends helper {
 		$pow = min($pow, count($units) - 1);
 		$bytes /= pow(1024, $pow);
 		return round($bytes, $precision) . ' ' . $units[$pow].'B';
+		}
+	static public function tmp($prefix = DIR_STORAGE, $suffix = '', &$token)
+		{
+		do $token = str::random( str::alphanumeric, self::$tmpfile_length );
+		while ( file_exists($prefix.$token.$suffix) );
+		
+		return $prefix.$token.$suffix;
+		}
+	static public function __callStatic($method, $args)
+		{
+		$count = (isset($args[0])) ? $args[0] : 1;
+		return $count * pow(1024, self::$sizes[$method]);
 		}
 }
